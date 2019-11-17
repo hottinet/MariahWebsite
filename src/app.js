60ec5295f6bc3be51f75430b497f1a4886fe8ddf
Casey Bradford <caseyebradford@gmail.com>
2019-11-17 13:27:00 -0500

    set up appContainer

--- src/app.js
+++ src/app.js
@@ -1,23 +1,37 @@
 import React from 'react'
+import injectSheet from 'react-jss'
 import { render } from 'react-dom'
 import { BrowserRouter, Route, Switch, withRouter } from 'react-router-dom'
 import Home from 'pages/Home'
 
-const App = ({ location }) => {
+const styles = {
+	wholePage: {
+		display: 'flex',
+		justifyContent: 'center',
+	},
+	appContainer: {
+		maxWidth: 1366,
+		width: '100%',
+	},
+}
+
+const App = ({ location, classes }) => {
 	// Create global effects or state here
 	// with access to router location
 
 	return (
-		<>
-			<Switch>
-				<Route path="/" exact component={Home} />
-			</Switch>
-			{/* NavBar?? */}
-		</>
+		<div className={classes.wholePage}>
+			<div className={classes.appContainer}>
+				<Switch>
+					<Route path="/" exact component={Home} />
+				</Switch>
+				{/* NavBar?? */}
+			</div>
+		</div>
 	)
 }
 
-const RouterApp = withRouter(props => <App {...props} />)
+const RouterApp = injectSheet(styles)(withRouter(props => <App {...props} />))
 
 render(
 	<BrowserRouter>
