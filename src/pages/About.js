e59634a5b751d4eb32a7e0fa02bf02d74f8fd814
Casey Bradford <caseyebradford@gmail.com>
2019-12-11 11:05:12 -0500

    about page copy

--- src/pages/About.js
+++ src/pages/About.js
@@ -23,7 +23,11 @@ const About = ({ classes }) => (
 	<PageWrapper iconSrc={AboutIcon}>
 		<ContentBlock>
 			<Header>About Me</Header>
-			<DescriptionText>Mariah Adcox is a writer and MFA candidate in Creative Nonfiction at Columbia University in New York City. </DescriptionText>
+			<DescriptionText>
+				Email Address <br />
+				Brooklyn, New York <br />
+				Mariah Adcox is a writer and MFA candidate in Creative Nonfiction at Columbia University in New York City.
+			</DescriptionText>
 			<ImageAspectRatio>
 				<img src={Headshot} className={classes.imageWidth} alt="" />
 			</ImageAspectRatio>
